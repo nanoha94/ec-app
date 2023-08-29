246c18c181c91255b0c951cfe4172b8075461f61
nanoha <nanoha.creator@gmail.com>
2023-08-29 18:11:21 +0100

    フォーマット

--- src/reducs/products/operations.js
+++ src/reducs/products/operations.js
@@ -1,30 +1,41 @@
-import { FirebaseTimestamp, db } from "../../firebase"
+import { FirebaseTimestamp, db } from "../../firebase";
 
-export const productsRef = db.collection('products');
+export const productsRef = db.collection("products");
 
-export const saveProduct = ( name, description, category, gender, price, navigate ) => {
-    return async(dispatch) => {
-        const timestamp = FirebaseTimestamp.now();
-        const data = {
-            name,
-            description,
-            category,
-            gender,
-            price: parseInt(price, 10),
-            updated_at: timestamp
-        }
+export const saveProduct = (
+  name,
+  description,
+  category,
+  gender,
+  price,
+  images,
+  navigate
+) => {
+  return async (dispatch) => {
+    const timestamp = FirebaseTimestamp.now();
+    const data = {
+      name,
+      description,
+      category,
+      gender,
+      price: parseInt(price, 10),
+      images,
+      updated_at: timestamp,
+    };
 
-        const ref = productsRef.doc();
-        const id = ref.id;
-        data.id = id;
-        data.created_at = timestamp;
+    const ref = productsRef.doc();
+    const id = ref.id;
+    data.id = id;
+    data.created_at = timestamp;
 
-        return productsRef.doc(id).set(data)
-        .then(() => {
-            navigate('/');
-        })
-        .catch((error) => {
-            throw new Error(error);
-        })
-    }
-}
+    return productsRef
+      .doc(id)
+      .set(data)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  };
+};
