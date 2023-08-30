350266271478b14eadd9f19df509965532e4a946
nanoha <nanoha.creator@gmail.com>
2023-08-30 11:17:31 +0100

    ファイルパスを変更

--- src/components/products/ImageArea.jsx
+++ src/components/products/ImageArea.jsx
@@ -2,7 +2,7 @@ import React, { useCallback } from "react";
 import { IconButton } from "@material-ui/core";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { styled } from "styled-components";
-import { storage } from "../../../firebase";
+import { storage } from "../../firebase";
 import ImagePreview from "./ImagePreview";
 
 const StyledIconButton = styled(IconButton)`
