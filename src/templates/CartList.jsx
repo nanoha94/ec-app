39f2292e02829e935a770d9f9a42c0f66c850cb3
nanoha <nanoha.creator@gmail.com>
2023-09-02 07:14:04 +0100

    カートページを作成

--- src/templates/CartList.jsx
+++ src/templates/CartList.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { List } from "@material-ui/core";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CartListItem } from "../components/products";
+import { GrayButton, PrimaryButton } from "../components/UIKit";
+import { useNavigate } from "react-router";
 
 const CartList = () => {
-  return <div>CartList</div>;
+  const navigate = useNavigate();
+  const usersSelector = useSelector((state) => state.users);
+  const ProductsInCart = usersSelector.cart;
+
+  const goToOrder = useCallback(() => {
+    navigate("/order/confirtm");
+  }, []);
+
+  const backToHome = useCallback(() => {
+    navigate("/");
+  }, []);
+
+  return (
+    <section className="c-section-wrapin">
+      <h2 className="u-text__headline">ショッピングカート</h2>
+      <List>
+        {ProductsInCart.length > 0 &&
+          ProductsInCart.map((product) => (
+            <CartListItem key={product.cartId} product={product} />
+          ))}
+      </List>
+      <div className="module-spacer--medium" />
+      <div className="p-grid__column">
+        <PrimaryButton label={"レジへ進む"} onClick={goToOrder} />
+        <div className="module-spacer--extra-extra-small" />
+        <GrayButton label={"ショッピングを続ける"} onClick={backToHome} />
+      </div>
+    </section>
+  );
 };
 
 export default CartList;
